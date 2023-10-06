c61405aab391c1e5ca991aa6b3e1331455c87d22
Roman Nemilov <nemilov220@gmail.com>
2023-10-06 14:55:46 +0300

    Добавлена функция взлома казино
    Пока удаётся взломать только 1 200 000 рублей

--- BenisConsole/BenisConsole/Program.cs
+++ BenisConsole/BenisConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BenisConsole
@@ -10,6 +12,18 @@ namespace BenisConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вы только что открыли приложение для взлома казино, вы уверены что хотите забрать у казино почти все его деньги? [y/n]");
+            Console.ReadLine();
+            Console.Write("Взлом:");
+            string vzlom = "[..........]100%";
+            for (int i = 0; i < vzlom.Length; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(vzlom[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Взлом завершён, получилось забрать: 1 200 000 рублей");
+            Console.ReadLine();
         }
     }
 }
